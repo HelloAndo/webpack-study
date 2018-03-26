4df366033c905a00daae3a8d52e9dbff3ff855c5
HelloAndo <331754520@qq.com>
2018-03-26 23:12:58 +0800

    nodemon监听webpack配置文件自动重启项目影响了热更新，先去掉

--- dev-server.js
+++ dev-server.js
@@ -7,8 +7,23 @@ const PORT = require('./config').dev.port
 
 const app = express()
 const compiler = webpack(webpackConfig)
+
+
+var hotMiddleware = require('webpack-hot-middleware')(compiler, {
+  log: () => {}
+})
+
 app.use(require('webpack-dev-middleware')(compiler))
-app.use(require('webpack-hot-middleware')(compiler))
+// app.use(require('webpack-hot-middleware')(compiler))
+app.use(hotMiddleware)
+
+// force page reload when html-webpack-plugin template changes
+compiler.plugin('compilation', function (compilation) {
+  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+    hotMiddleware.publish({ action: 'reload' })
+    cb()
+  })
+})
 
 app.use(express.static('.'))
 
